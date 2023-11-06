be5f4b27d1efb24c78eac285cc1dfb433777d0ab
imacakee <maripaolopezgomez@gmail.com>
2023-11-05 23:02:49 -0300

    added updateProduct & deleteProduct

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
+const fs = require("fs");
+const path = "./products/products.txt";
+
 class ProductManager {
-  constructor() {
-    this.products = [];
+  constructor(path) {
+    this.path = path;
+    this.products = this.fetchData();
+  }
+
+  fetchData() {
+    try {
+      const fileContent = fs.readFileSync(this.path, "utf-8");
+      if (fileContent?.length) {
+        return JSON.parse(fileContent);
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.log(`no se encontró el archivo: ${error}`);
+      return [];
+    }
+  }
+
+  writeData() {
+    fs.writeFileSync(this.path, JSON.stringify(this.products));
   }
 
   getProducts() {
-    return this.products;
+    return this.fetchData();
   }
 
   getProductById(id) {
     return (
-      this.products.find((prd) => prd.id == id) || console.log("not found")
+      this.fetchData().find((prd) => prd.id == id) || console.log("not found")
     );
   }
 
@@ -21,18 +43,37 @@ class ProductManager {
       return;
     }
 
-    if (this.products.length > 0) {
-      product.id = this.products[this.products.length - 1].id + 1;
+    const productsList = this.fetchData();
+
+    if (productsList.length > 0) {
+      product.id = productsList[productsList.length - 1].id + 1;
     } else {
       product.id = 1;
     }
 
-    if (this.products.some((prd) => prd.code == product.code)) {
+    if (productsList.some((prd) => prd.code == product.code)) {
       console.log(`el código ${product.code} ya existe`);
-      return;
+    } else {
+      productsList.push(product);
+      this.products = productsList;
+      this.writeData();
     }
+  }
 
-    this.products.push(product);
+  updateProduct(id, newProduct) {
+    let prod = this.getProductById(id);
+    prod = { ...prod, ...newProduct, id };
+    const productsList = this.fetchData();
+    const foundIndex = productsList.findIndex((prd) => prd.id == id);
+    productsList[foundIndex] = prod;
+    this.products = productsList;
+
+    this.writeData();
+  }
+
+  deleteProduct(id) {
+    this.products = this.fetchData().filter((product) => product.id != id);
+    this.writeData();
   }
 }
 
